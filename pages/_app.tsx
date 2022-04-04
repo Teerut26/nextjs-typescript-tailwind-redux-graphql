de2d9631ee41b7ffa11640476cc13ca6dddde3f4
Teerut26 <teerutsr@gmail.com>
2022-04-04 23:06:55 +0700

    update components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { wrapper } from '../store';
+import { wrapper } from "../store";
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="nextjs-typescript-tailwind-redux-graphql" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
-export default wrapper.withRedux(MyApp)
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <title>Create Next App</title>
+                <meta name="nextjs-typescript-tailwind-redux-graphql" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+};
+
+export default wrapper.withRedux(MyApp);
